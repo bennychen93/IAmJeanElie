9fe47faf371a7947534ab0f908dea439ca91e8e5
saasbook <saasbook@saasbook.(none)>
2014-04-02 18:18:58 -0300

    Finished setting up Devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project::Application.config.secret_token = '20c4c0f092ab8d49568f3b9272d06796a91e0bcc2bff2d396af1cbb04656ee1ad65eff36fc1b192e9d13b385715c43e8d535dd5fee9fecc0e17c46a223469ba9'
