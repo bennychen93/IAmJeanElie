b9884a58bc111cb45f7bb3a2f118f0366c71e81d
saasbook <saasbook@saasbook.(none)>
2014-04-09 19:05:29 -0300

    Trying to get confirmation page to work

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140402204833) do
+ActiveRecord::Schema.define(:version => 20140409211226) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
