aed8f6edbdba60a9d068f9232dba031e501a0302
saasbook <saasbook@saasbook.(none)>
2014-04-28 19:04:34 -0300

    working on sign_up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   def after_sign_in_path_for(resource)
-  	u = params[:user].confirmed?
-  	if u
+  	u = User.find(current_user).confirmed?
+  	if !u
   		flash[:confirm] = "you have not confirm yet!"
   		new_user_session_path
   	else
