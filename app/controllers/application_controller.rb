1a28f2c0ee232aab8bef04519011c5734a28504e
saasbook <saasbook@saasbook.(none)>
2014-04-28 18:55:07 -0300

    working on sign_up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   def after_sign_in_path_for(resource)
-  	edit_user_registration_path
+  	u = params[:user].confirmed?
+  	if u.nil?
+  		flash[:confirm] = "you have not confirm yet!"
+  		new_user_session_path
+  	else
+  		edit_user_registration_path
+  	end
   end
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,12 +1,14 @@
 class RegistrationsController < Devise::RegistrationsController
   protected
   def after_sign_up_path_for(resource)
-    new_user_confirmation_path
+  	flash[:confirm] = "Please check your email for confirmation"
+    new_user_session_path
   end
   protected
 
   def after_inactive_sign_up_path_for(resource)
-    new_user_confirmation_path
+  	flash[:confirm] = "Please check your email for confirmation"
+    new_user_session_path
   end
 end
 
